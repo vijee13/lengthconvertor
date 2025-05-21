887112dfb4aae93bf15cf70caba89a7fed3701ac
Vijayalakshmi Sathishkumar <151650449+vijee13@users.noreply.github.com>
2025-05-21 15:02:24 +0530

    Add files via upload

---
+++ scriptlength.js
@@ -0,0 +1,10 @@
+
+function convert() {
+
+    let cmVal = Number(document.getElementById("input").value);
+    let inchVal = cmVal/2.54;
+    let result = document.getElementById("result");
+    result.innerHTML = inchVal.toFixed(2) + " inches";
+   
+
+}
